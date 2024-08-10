53effcb569f49e22056619c21ea4fae7c5a9778f
mjagadeesh <mamilla.jagadeesh@soti.net>
2024-08-10 15:02:39 +0530

    Episode2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
@@ -6,7 +9,7 @@ const parent = React.createElement(
     "div",
     {id:"parent"},
     React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h1",{},"I am h1 tag hello"),
         React.createElement("h2",{},"I am h2 tag")
     ])
 )
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 #React Concepts
+
+#Parcel
+- Dev Build
+- Local Server
+- HMR(hot module replacement) // reloads automatically
+- File watching algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bndling
+- Compress
+- Consistent Hashing
+- Code splitting
+- 
