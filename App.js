ed6bcc92534354076735a424d547a88931e76f7c
mjagadeesh <mamilla.jagadeesh@soti.net>
2024-08-11 21:12:04 +0530

    Episode3

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+// React.createElement => Object => HTML element
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"I am h1 tag hello"),
-        React.createElement("h2",{},"I am h2 tag")
-    ])
-)
+// JSX it is not react
+// it is JSX not html, it is HTML or XML like syntax
+// use brackets
+const JsxHeading  = () => (<h1 className="head" id="heading">
+    NamasteReact Using JSX🚀
+    </h1>);
+const react = <h1>React</h1>
+const name = <h1>Hi {react} Element</h1>;
+
+// ReactComponents
+// Class based components - old way - uses js classes
+// Functional based components - new way - uses js functions
+
+//React Functional Component ==> a function which returna jsx 
+const Heading = ()=>{
+    return (<div>
+        <JsxHeading/> {/*  component Composition */}
+        {name}
+        <h1>Namaste React Functional Component</h1>
+        </div>);
+};
+
+// const heading2 = () => <h1>Namaste React Functional Component</h1>; same as above
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(jsxHeading);
+root.render(<Heading/>);
